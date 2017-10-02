6687f3fae7345a4c767c61555e2ab98a54128c6a
Home <Home@DESKTOP-KJ5N5AK>
2017-10-02 23:02:52 +0100

    First commit

---
+++ src/main/java/com/home/Spring/MVC/Project/SpringMvcProjectApplication.java
@@ -0,0 +1,12 @@
+package com.home.Spring.MVC.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProjectApplication.class, args);
+	}
+}
